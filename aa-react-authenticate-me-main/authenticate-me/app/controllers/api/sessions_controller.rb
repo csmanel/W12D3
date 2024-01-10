89ed1943f0146cabc5a4d2760bc776467d9cccd1
csmanel <csmanel@gmail.com>
2024-01-10 10:00:09 -0500

    onto phase three!

--- aa-react-authenticate-me-main/authenticate-me/app/controllers/api/sessions_controller.rb
+++ aa-react-authenticate-me-main/authenticate-me/app/controllers/api/sessions_controller.rb
@@ -1,10 +1,19 @@
 class Api::SessionsController < ApplicationController
   def show
+    @user = current_user 
+    @user ? render 'api/users/show' : render json: { user: nil }
+    end
   end
 
   def create
+    username = params[:username]
+    password = params[:password]
+    @user = user.find_by_credentials(username, password)
+    @user ? render 'api/users/show' :  render json: { errors: ['Invalid credentials'] }, status: 422
   end
 
   def destroy
+    logout
+    head :no_content
   end
 end
