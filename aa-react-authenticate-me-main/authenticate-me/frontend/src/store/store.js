204e71bd73f00e4e48f59cac7c71ce2beb321d32
csmanel <csmanel@gmail.com>
2024-01-12 14:17:41 -0500

    finish proj

--- aa-react-authenticate-me-main/authenticate-me/frontend/src/store/store.js
+++ aa-react-authenticate-me-main/authenticate-me/frontend/src/store/store.js
@@ -1,15 +1,16 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session';
 
 const rootReducer = combineReducers({
-  // ADD REDUCERS HERE
+  session,
 });
 
 let enhancer;
-if (import.meta.env.MODE === "production") {
+if (import.meta.env.MODE === 'production') {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = (await import("redux-logger")).default;
+  const logger = (await import('redux-logger')).default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
